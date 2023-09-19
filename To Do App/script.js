c3101bf7e33a2b2e3d87bae26cd5233bf0cc1797
mgkrishnangopal <135576063+mgkrishnangopal@users.noreply.github.com>
2023-09-19 18:49:07 +0530

    Add files via upload

---
+++ To Do App/script.js
@@ -0,0 +1,99 @@
+const inputtask=document.querySelector(".wrapper .task-name");
+const addtask=document.querySelector(".wrapper .add-btn");
+const cleartask=document.querySelector(".wrapper .clear-all");
+const tasklistt=document.querySelector(".to-do-app .list-of-tasks");
+
+const notcompletedIcon=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg>`;
+
+const completedIcon=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
+<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+<path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
+</svg>`;
+const edittaskIcon=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg>`;
+const deltaskIcon=`
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+<path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+</svg>`;
+
+
+const list_of_items=[];
+const addNewTask= (tasktitle,completestatus) => {
+if(tasktitle===""){
+    console.warn("Inputs Cannot Be Empty");
+    return;
+};
+
+
+list_of_items.push({
+tasktitle,
+completestatus
+});
+console.log(list_of_items);
+};
+
+const displayTasks = () => {
+if (list_of_items.length === 0) {
+    tasklistt.textContent = " You Don't Have Any Pending Tasks.......!";
+    return;
+}
+tasklistt.innerHTML="";
+
+list_of_items.forEach(e => {
+     
+    const title= document.createElement("section");
+    title.classList.add("task-title");
+    title.innerHTML=e.tasktitle;
+
+    const delBtn = document.createElement("button");
+    delBtn.classList.add("del-btn");
+    delBtn.innerHTML =deltaskIcon;
+
+    const EditBtn= document.createElement("section");
+    EditBtn.classList.add("edit-btn");
+    EditBtn.innerHTML=e.edittaskIcon;
+    
+    delBtn.addEventListener("click", () => {
+            list_of_items.splice(list_of_items.indexOf(e), 1);
+            displayTasks();
+        });
+        EditBtn.addEventListener("click", () => {
+            tasklistt.textContent=list_of_items.indexOf(e);
+            displayTasks();
+        });
+
+
+
+  
+    const tasks=document.createElement("section");
+    tasks.classList.add("tasks");
+
+    tasks.appendChild(title);
+    tasks.appendChild(delBtn);
+    tasks.appendChild(EditBtn);
+tasklistt.appendChild(tasks);
+console.log(tasklistt);
+});
+
+};
+
+
+
+
+addtask.addEventListener ("click", () => {
+let newtask = inputtask.value ;
+addNewTask(newtask,false);
+
+ displayTasks();
+
+inputtask.value= "";
+inputtask.focus();
+
+
+
+});
